bf98c420e9e01e5f042478aacbdc5794bce3714d
Anburaj S <anburaj2203@gmail.com>
2024-05-09 12:25:02 +0530

    added states switching fucntionality

--- client/src/components/Content.jsx
+++ client/src/components/Content.jsx
@@ -5,16 +5,41 @@ import GmailList from "./GmailList";
 
 const Content = () => {
   const { data: gmails, isLoading, isSuccess } = useGetGmailsQuery();
-  const [state, setState] = useState("Primary");
+  const [state, setState] = useState("primary");
+  let promotion = [];
+  let primary = [];
+  let social = [];
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center m-auto h-screen">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+      </div>
+    );
+  }
+
+  if (isSuccess) {
+    promotion = gmails.filter((el) => el.category == "promotion");
+    social = gmails.filter((el) => el.category == "social");
+    primary = gmails.filter(
+      (el) => el.category != "promotion" && el.category != "social"
+    );
+  }
+  console.log(promotion, "promotion");
+  console.log("primary:", primary);
+
+  let data =
+    state === "primary" ? primary : state === "promotion" ? promotion : social;
+
   return (
     <div className="w-[83.5%] py-4 rounded-2xl bg-[#e1d9dd]">
       <div className="flex justify-between p-2">
         <div>◻️ 🔄️ </div>
-        {isSuccess && <div>Total: {gmails.length} </div>}
+        {isSuccess && <div>Total: {data.length} </div>}
       </div>
       <div className={`w-[50%] text-left flex items-start `}>
         <div
-          onClick={() => setState("Primary")}
+          onClick={() => setState("primary")}
           className={`w-[33%] p-2 hover:bg-[#d0c3c5] ${
             state == "Primary" &&
             "font-semibold text-[#5030E5] border-b-4 border-indigo-900"
@@ -23,7 +48,7 @@ const Content = () => {
           <button>🖼️ Primary</button>
         </div>
         <div
-          onClick={() => setState("Promotions")}
+          onClick={() => setState("promotion")}
           className={`w-[33%] p-2 hover:bg-[#d0c3c5] ${
             state == "Promotions" &&
             "font-semibold text-[#5030E5] border-b-4 border-indigo-900"
@@ -32,7 +57,7 @@ const Content = () => {
           <button>🧷 Promotions</button>
         </div>
         <div
-          onClick={() => setState("Social")}
+          onClick={() => setState("social")}
           className={`w-[33%] p-2 hover:bg-[#d0c3c5] ${
             state == "Social" &&
             "font-semibold text-[#4123c5] border-b-4 border-indigo-900"
@@ -41,7 +66,7 @@ const Content = () => {
           <button>👪 Social</button>
         </div>
       </div>
-      <GmailList data={gmails} />
+      <GmailList data={data} />
     </div>
   );
 };
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import profile from "../../images/photo.jpg";
-import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
   const [search, setSearch] = useState("");
   const navigate = useNavigate();
@@ -9,10 +9,11 @@ const Navbar = () => {
   const handleSearch = () => {
     console.log(search);
   };
+
   return (
-    <DIV className="flex items-center justify-between w-[100%] p-4 text-left">
+    <div className="flex items-center justify-between w-[100%] p-4 text-left">
       <div className="w-[65%] flex justify-around ">
-        <div className=" w-[25%] flex items-center justify-center ">
+        <div className="w-[25%] flex items-center justify-center">
           <h2
             className="text-xl font-normal text-white hover:cursor-pointer"
             onClick={() => navigate("/")}
@@ -20,23 +21,25 @@ const Navbar = () => {
             Gmail
           </h2>
         </div>
-        <div className="rounded-full flex justify-between px-4 items-center bg-white w-[75%]">
-          <div className=" flex items-center">
-            <button>🔍</button>
+        <div className="rounded-full h-12 flex justify-between px-4 items-center bg-[#c6c3c3] w-[75%] focus-within:bg-white">
+          <div className="w-[96%] flex items-center">
+            <button className="w-[6%]">🔍</button>
             <input
               type="text"
-              className="w-[50%] font-normal text-lg ml-2"
+              className="w-[100%] font-normal text-lg ml-2 placeholder-slate-50 bg-[#c6c3c3] focus:bg-white focus:outline-none focus:placeholder-black"
               placeholder="Search mail"
               value={search}
               onChange={(e) => setSearch(e.target.value)}
               onKeyDown={(e) => {
-                if (e.key == "Enter") {
+                if (e.key === "Enter") {
                   handleSearch();
                 }
               }}
             />
           </div>
-          <button onClick={() => setSearch("")}>✖️</button>
+          <button className="w-[4%]" onClick={() => setSearch("")}>
+            ✖️
+          </button>
         </div>
       </div>
       <div className="w-[17%] flex justify-around items-center">
@@ -45,14 +48,9 @@ const Navbar = () => {
         <button>✉️</button>
         <img src={profile} alt="pic" className="rounded-full w-10 h-10" />
       </div>
-    </DIV>
+    </div>
   );
 };
 
 export default Navbar;
 
-const DIV = styled.div`
-  div {
-    height: 50px;
-  }
-`;
